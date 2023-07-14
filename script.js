52c811e8a860ba3b7dd42ba65d1d08bf4d34d8a9
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-07-14 08:00:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 //complete this code
 class Animal {
-	constructor(species){
-		this.species = species;
-		getSpecies(){
-			return this.species;
-		}
-		makeSound(){
-			console.log("sound")
-		}
-	}
-}
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {
-	purr(){
-		console.log("purr");
-	}
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
 }
 
 class Cat extends Animal {
-	bark(){
-		console.log("woof");
-	}
+  purr() {
+	  
+    console.log("purr");
+  }
 }
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+
 
 // Do not change the code below this line
 window.Animal = Animal;
